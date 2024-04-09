7843b1c880a42ca8aaf14d6b6fcd52a51acd6c67
pay2joy2 <73960156+pay2joy2@users.noreply.github.com>
2024-04-09 13:58:34 +0300

    Delete_wrong_commit

--- src/by/it/group251051/smalianko/lesson12/SplayNode.java
+++
@@ -1,8 +0,0 @@
-package by.it.group251051.smalianko.lesson12;
-
-public class SplayNode{
-    Integer key;
-    String value;
-    SplayNode parent, left, right;
-    SplayNode(Integer k){key = k; value = null; parent = left = right = null;}
-}
